c089d5ef0a3944d1ea61c2f6fca2f5a0b2f47d00
pmod23@gmail.com <you@example.com>
2020-10-14 12:10:15 -0400

    Commit 1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Project4FlashCards
+
+// Array of All Questions
+const questions = [
+{ state: 'Florida', capital: 'Talahassee' },
+{ state: 'Maryland', capital: 'Annapolis' },
+]
---
+++ script.js
@@ -0,0 +1,25 @@
+// Start Game
+
+    // Remove Splash Screen
+
+    // Display Question
+
+    // Set Progress bar
+
+
+// Playling The Game
+
+    // IF
+        // last question AND there are no incorrect ones left, then end the game
+
+    // ELSE
+
+        // Flip the card
+
+        // Log Answer/Score
+
+        // Update The Score
+
+        // Go To next card
+
+        // Update Progress
